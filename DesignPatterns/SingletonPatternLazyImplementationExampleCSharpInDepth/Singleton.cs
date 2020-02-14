1ac844dca62cf6ab728b848ca964a2531645e545
mindball <mincho.balaliew@gmail.com>
2020-02-14 10:26:18 +0200

    Singleton

---
+++ DesignPatterns/SingletonPatternLazyImplementationExampleCSharpInDepth/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPatternLazyImplementationExampleCSharpInDepth
+{
+    public sealed class Singleton
+    {
+
+        //https://csharpindepth.com/articles/singleton#lazy
+        /*
+         * 
+         * It's simple and performs well. It also allows you to check whether or not the instance
+         * has been created yet with the IsValueCreated property.
+         * 
+         * */
+        private static readonly Lazy<Singleton> lazy =
+            new Lazy<Singleton>(() => new Singleton());
+
+        private Singleton()
+        {
+        }
+        public static Singleton Instance { get { return lazy.Value; } }
+    }
+}
---
+++ SolidPrinciples/DependancyInjectionContainer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DependancyInjectionContainer
+{
+    class Program
+    {
+        /*
+         * IoC Service lifetimes:
+         *      Transient: the service is created each time it is requested
+         *      Singleton: created once for the lifetime of the application
+         *      Scoped: created once per request
+         *  Benefit of the container: lifestyle management
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * */
+        static void Main(string[] args)
+        {
+            CastleWindsorIoC newObj = new CastleWindsorIoC();
+
+        }
+    }
+}
